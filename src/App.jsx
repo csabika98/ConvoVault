70895fd837d418714c50826c4a53cf4cd837a2e3
andrastothinnoveo <andras.toth@innoveo.com>
2026-04-29 10:18:41 +0200

    init base layout and History component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import History from "./components/History/History";
+
+
 function App() {
   return (
-    <>    
+    <>
+      <ResizablePanelGroup
+        orientation="horizontal"
+        className="h-screen w-full rounded-lg border"
+      >
+        <ResizablePanel defaultSize={20}>
+          <div className="flex h-full items-center justify-center p-6">
+            {/*<span className="font-semibold">Sidebar</span>*/}
+            <History />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={60}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Chatbox</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={20}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Profile</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </>
   )
 }
---
+++ src/components/History/History.jsx
@@ -0,0 +1,20 @@
+import SessionCard from "./SessionCard";
+import { Separator } from "@/components/ui/separator";
+
+function History() {
+
+  return (
+    <>
+      <div className="w-full min-h-screen bg-background p-1">
+        <h1 className="mb-4">History</h1>
+        <Separator className="mb-6" />
+          <div className="flex flex-col gap-4 max-w-6xl mx-auto w-full">
+            <SessionCard counter="1"/>
+            <SessionCard counter="2"/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default History;
